699cb802799343f13f275ced553bca4f524e3ed7
YOOHOO <88616418+YOOHOO0@users.noreply.github.com>
2022-05-19 01:06:59 +0900

    Update ft_ultimate_range.c

--- c07/ex02/ft_ultimate_range.c
+++ c07/ex02/ft_ultimate_range.c
@@ -34,28 +34,3 @@ int	ft_ultimate_range(int **range, int min, int max)
 	*range = numarr;
 	return (i);
 }
-
-/*
-#include <stdio.h>
-int main()
-{
-	int min = -3;
-	int	max = 6;
-	int *arr;	
-	printf("min :%d  max: %d\n\n",min, max);
-	int result =ft_ultimate_range(&arr, min, max);
-	printf("result:::::: len :%d \n\n\n\n",result);
-	if (result <= 0)
-	{
-		printf("ex02 error check is ok\n");
-	}
-	else
-	{
-		for (int i = 0; i < result; i++)
-		{
-			printf("arr[%d] : %d\n", i,arr[i]);
-		}
-		free(arr);
-	}
-}
-*/
